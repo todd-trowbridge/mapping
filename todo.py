8c0f773f8b3b7d26b2c0ee03ed0fcc3cf0a0e95f
Todd Trowbridge <todd.trowbridge@icloud.com>
2021-06-08 13:24:41 -0400

    formatting

--- todo.py
+++ todo.py
@@ -4,30 +4,25 @@ import json
 # json file to open
 file_name = "todo.json"
 
-#create empty dictionary
-data = {}
-
-# open the file todo.json and load it to the data variable
+# open the file todo.json and load it to the variable 'data'
 with open(file_name, "r") as file_handle:
     data = json.load(file_handle)
 
 def add_todo():
   # get number of todos
   count = len(data)
-  
   # set key to number of todos plus 1
   key = count + 1
+  # ask the user for a new todo
   value = input("What's your todo? ")
-
-  # add the task
+  # add the todo
   data[key] = value
-
-  # save the data
+  # save the data to a file
   with open(file_name, "w") as file_handle:
     json.dump(data, file_handle)
 
 # try to add a todo
 add_todo()
 
-# check if the addition worked
+# check if adding the todo worked
 print(data)
