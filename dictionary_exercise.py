b71822fad57aa9ffeac43eb63d2c503528cabc0f
Todd Trowbridge <todd.trowbridge@icloud.com>
2021-06-08 23:33:46 -0400

    finished dictionary_exercise

--- dictionary_exercise.py
+++ dictionary_exercise.py
@@ -11,29 +11,29 @@ Happy Holidays!
 
 # functions
 def add_a_present():
-  user_response = input("What would you like from Santa this year or if you can't think of anymore type 'done'\n")
+  user_response = input("What would you like from Santa this year?\n(type done to exit)\n")
   if user_response != 'done':
     data['requested_gifts'].append(user_response)
-    print(data)
     add_a_present()
 
 def ask_if_good_or_bad():
-  user_response = input("Have you been good or bad this year?\n")
+  user_response = input("Have you been good or bad this year? ")
   if user_response.lower() == 'good':
+    print("\nThat's wonderful to hear!  I'm going to bring you...")
     for present in data['requested_gifts']:
-      print("That's wonderful to hear!  I'm going to bring you...")
       print(f'a {present}')
   elif user_response.lower() == 'bad':
     print("Oh, you've been naughty?  Expect some coal.")
   else:
-    data['requested_gifts'].append('an electronic spell checker')
-    print("(sigh) I've added an electronic spell checker to your list of gifts.")
+    data['requested_gifts'].append('spell checker')
+    print("(sigh) you mispelled a word`")
+    ask_if_good_or_bad()
 
 # print greeting
 print(greeting)
 
-# run recursive add presents
+# runs recursively
 add_a_present()
 
-# list presents or coal
+# runs recursively
 ask_if_good_or_bad()
